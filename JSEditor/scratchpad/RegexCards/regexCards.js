05347d000acf3ecc82d562c1ebe388e8cd91539b
Daniel Bermender <daniel.bermender@dliflc.edu>
2015-04-20 21:07:44 -0500

    RegexCards -Another backup

--- JSEditor/scratchpad/RegexCards/regexCards.js
+++ JSEditor/scratchpad/RegexCards/regexCards.js
@@ -11,56 +11,52 @@ var jConfig
 var jDecks = $("<decks></decks>")
 var jCards = $("<cards></cards>")
 var jMatches = $("<matches></matches>")
+var currentMatch = 0
 
 function loadFiles(){
-	var loadedAll = false
-
-	$.get(mediaPath + "config.xml", function(t_config){
+	get(mediaPath + "config.xml", function(t_config){
 		jConfig = $(t_config)
 
 		//Load decks
-		$.get(mediaPath + "decks/" , function(t_decksDir){
+		get(mediaPath + "decks/" , function(t_decksDir){
 			var deckFileList = getFileLinksFromDirectoryListing(t_decksDir)
-			//startGame()
 
 			$.each(deckFileList, function(i,v){
-				$.get(mediaPath + "decks/" + v , function(t_decks){
-					jDecks.append($(t_decks).find("deck"))
+				get(mediaPath + "decks/" + v , function(t_deck){
+					var jdeck = $(t_deck)
+					jdeck.attr("name", removeFileExt(v))
+					jDecks.append(jdeck)
 				})
 			})
-		})	
 
-		//Load cards
-		$.get(mediaPath + "cards/" , function(t_cardsDir){
-			var cardFileList = getFileLinksFromDirectoryListing(t_cardsDir)
-			//startGame()
+			//Load cards
+			get(mediaPath + "cards/" , function(t_cardsDir){
+				var cardFileList = getFileLinksFromDirectoryListing(t_cardsDir)
 
-			$.each(cardFileList, function(i,v){
-				$.get(mediaPath + "cards/" + v , function(t_cards){
-					jCards.append($(t_cards).find("cards"))
+				$.each(cardFileList, function(i,v){
+					get(mediaPath + "cards/" + v , function(t_card){
+						jCards.append($(t_card))
+					})
 				})
-			})
+
+				//Load matches
+				get(mediaPath + "matches/" , function(t_matchesDir){
+					var matchFileList = getFileLinksFromDirectoryListing(t_matchesDir)
+
+					$.each(matchFileList, function(i,v){
+						get(mediaPath + "matches/" + v , function(t_match){
+							jMatches.append($(t_match))
+						})
+					})
+
+					startGame()
+				})	
+
+			})	
 		})	
-
-		//Load matches
-		$.get(mediaPath + "matches/" , function(t_matchesDir){
-			var matchFileList = getFileLinksFromDirectoryListing(t_matchesDir)
-			//startGame()
-
-			$.each(matchFileList, function(i,v){
-				$.get(mediaPath + "matches/" + v , function(t_matches){
-					jMatches.append($(t_matches).find("matches"))
-				})
-			})
-		})	
-
 	})
 }
 
-var cards = {
-	singleLetter:"singleLetter",
-	doubleLetter:"doubleLetter"
-}
 
 var whoseTurn 
 
@@ -71,6 +67,11 @@ function startGame(){
 	if(params['numPlayers'] != undefined){
 		numPlayers = params['numPlayers']
 	}
+
+	var phraseArr = $(jMatches.find("match")[currentMatch]).find("phrase").text()
+	$.each(phraseArr, function(i,v){
+		$("#phrase").append("<div class='letter'>" + v + "</div>")
+	})
 
 	for(var i=0; i < numPlayers; i++){
 		var jPlayer = $($("#snippet_playerContainer").html())
@@ -84,8 +85,12 @@ function startGame(){
 }
 
 var potentialRollPercent = .5
+var jCurrentDeck
 
 function handleTurn(){
+	jCurrentDeck = jDecks.find("deck[name='" 
+			+ $(jConfig.find("player")[whoseTurn]).attr("deck") + "']")
+
 	$(".playerContainer").removeAttr("turn")
 	
 	$($(".playerContainer")[whoseTurn]).attr("turn","")
@@ -105,12 +110,12 @@ function handleTurn(){
 			parseInt($($("#main .playerPotentials")[whoseTurn]).text()) + potentalRoll)
 	
 	//Handle cards
-	var cardName = randomCard()
+	var j_card = randomCard()
 
-	var jCard = $($("#snippet_card").html())
-	jCard.text(cardName)
-	$($(".playerCards")[whoseTurn]).append(jCard)
-	jCard.draggable({revert: "invalid",containment: "#main", scroll: false})
+	var j_card_snip = $($("#snippet_card").html())
+	j_card_snip.text(j_card.text())
+	$($(".playerCards")[whoseTurn]).append(j_card_snip)
+	j_card_snip.draggable({revert: "invalid",containment: "#main", scroll: false})
 	
 	//End turn
 	turnNum++	
@@ -127,14 +132,17 @@ function nextTurn(){
 }
 
 function randomCard(){
-	var numberCards = Object.keys(cards).length
+	var numberCards = jCurrentDeck.find("card").length
 	var cardIndex = getRandomInt(0, numberCards - 1)
 
-	if(Object.keys(cards)[cardIndex] == undefined){
+	var j_card = jCards.find("card[id='" 
+					+ $(jCurrentDeck.find("card")[cardIndex]).attr("id")
+					+ "']")
+	if( j_card.length == 0){
 		throw "Card undefined"
+	}else{
+		return j_card
 	}
-
-	return Object.keys(cards)[cardIndex]
 }
 
 var colors = ["red", "orange", "yellow", "green", "blue", "purple"]
@@ -286,4 +294,27 @@ function getFileLinksFromDirectoryListing(indexPageData){
 	return outputArr
 }
 
+function get(url, callback){
+	$.ajax({
+		    type: "GET",
+		    url: url,
+		    dataType: "text",
+			async: false,
+		    success: callback,
+		    error: ajaxErrorFunc
+		});
+}
+
+function ajaxErrorFunc(jqXHR, textStatus, errorThrown){
+	alert("Error: " + errorThrown);
+}
+
+function removeFileExt(filename){
+	//return /([\w]+)([.][\w]+$)/.exec(filename)[1];
+	if(/(.*)([.].+$)/.exec(filename) != undefined){
+		return /(.*)([.].+$)/.exec(filename)[1]
+	}else{
+		return filename;
+	}
+}
 //End Utils------------------------------------------------------
